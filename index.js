3a7ce2b823ffb4d3f228c2576e55cf48b1bc74c1
Bazargaziyev Nursultan <nursultanbazargaziyev@gmail.com>
2023-08-07 16:42:49 +0200

    Configured cors - now all resources can use my service

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { scan, getReport } = require("./controller");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/scan", scan);
 app.get("/report/:id", getReport);
